83992077d09f3adc1fceab06eed231479827fe42
Tyler James Devlin <devlintj@dcsdk12.org>
2016-09-08 13:38:49 -0600

    Tests for has_teen?

--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -2,4 +2,26 @@ require 'minitest/autorun'
 require './methods_quiz'
 
 # TODO - write tests here
+describe 'methods_quiz' do
 
+## has_teen
+  describe 'has_teen?' do
+    it 'should be true if a teen is present' do
+      has_teen?(2, 40, 13).must_equal(true)
+    end
+
+    it 'should be true if two teens are present' do
+      has_teen?(2, 19, 13).must_equal(true)
+    end
+
+    it 'should be true if all teens are present' do
+      has_teen?(19, 15, 13).must_equal(true)
+    end
+
+    it 'should be false if no teens are present' do
+      has_teen?(2, 40, 10).must_equal(false)
+    end
+  end
+
+
+end
